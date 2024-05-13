14fc608c96e98dbc779fd0daa0fd475f96d22047
orpinelli <ga.f.orpinelli@gmail.com>
2024-05-13 17:07:53 -0300

    feat: img home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 
-export default function Home() {
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api("/products/featured");
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts();
+  console.log(highlightedProduct.image, "img");
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
-          src="/graos-de-cafe.png"
+          src={highlightedProduct.image}
           className="group-hover:scale-105 trasnsition-trasnsform duration-500 rounded-lg"
           width={920}
           height={930}
@@ -17,52 +28,43 @@ export default function Home() {
           alt=""
         />
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate"> Coffee</span>
+          <span className="text-sm truncate"> {highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold ">
-            $200,00
+            {highlightedProduct.price.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
           </span>
         </div>
       </Link>
 
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          src="/graos-de-cafe.png"
-          className="group-hover:scale-105 trasnsition-trasnsform duration-500"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-        />
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate"> Coffee</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold ">
-            $200,00
-          </span>
-        </div>
-      </Link>
-
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          src="/graos-de-cafe.png"
-          className="group-hover:scale-105 trasnsition-trasnsform duration-500"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-        />
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate"> Coffee</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold ">
-            $200,00
-          </span>
-        </div>
-      </Link>
+      {otherProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              src={product.image}
+              className="group-hover:scale-105 trasnsition-trasnsform duration-500"
+              width={920}
+              height={920}
+              quality={100}
+              alt=""
+            />
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate"> {product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold ">
+                {product.price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
