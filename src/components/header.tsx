6788e06606edda53472162e8d50cb3b686f51a11
orpinelli <ga.f.orpinelli@gmail.com>
2024-06-24 18:26:47 -0300

    feat: SearchForm

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import { Search, ShoppingBag } from "lucide-react";
-import Image from "next/image";
-import { CartWidget } from "./cart-widget";
+import Link from 'next/link'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export default function Header() {
   return (
@@ -10,19 +11,11 @@ export default function Header() {
         <Link href="/" className="text-2x1 font-extrabold text-white">
           Coffee Next
         </Link>
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 border-zinc-500">
-          <Search className="w-5 h-5 text-zinc-500" />
-          <input
-            placeholder="Buscar Produto..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
-
       <div className="flex items-center gap-4">
         <CartWidget />
         <div className="w-px h-4 bg-zinc-700" />
-
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
           <Image
@@ -35,5 +28,5 @@ export default function Header() {
         </Link>
       </div>
     </div>
-  );
+  )
 }
